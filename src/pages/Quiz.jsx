91ca12756b8e80fb959a0a5e30ca89169d24bd9d
Jimmy <jimmy.ganci@gmail.com>
2021-10-22 14:07:32 +0200

    modified questions and answers

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -40,17 +40,17 @@ const Quiz = () => {
     setSliceVal2(sliceVal2 + 1);
   };
 
-  //   const shuffleArray = (array) => {
-  //     let i = array.length - 1;
-  //     for (; i > 0; i -= 1) {
-  //       const j = Math.floor(Math.random() * (i + 1));
-  //       const temp = array[i];
-  //       array[i] = array[j];
-  //       array[j] = temp;
-  //     }
-  //     return array;
-  //   };
-  //   shuffleArray(answersRandom);
+  const shuffleArray = (array) => {
+    let i = array.length - 1;
+    for (; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+  shuffleArray(answersRandom);
 
   return (
     <div className="quizContainer">
