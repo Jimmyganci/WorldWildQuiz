4d12e98bb6b9d6464aaf5d43f9d7688c952731b4
DavidVM19 <davidvm1910@gmail.com>
2021-10-29 10:10:03 +0200

    Initial search bar Culture

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Quiz from './pages/Quiz';
+import Culture from './pages/Culture';
 
 const App = () => {
   return (
     <div className="app">
       <Quiz />
+      <Culture />
     </div>
   );
 };
---
+++ src/pages/Culture.jsx
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import '../styles/culture.css';
+
+const SearchCulture = () => {
+  const [allCountries, setAllCountries] = useState([]);
+  const [searchCountry, setSearchCountry] = useState('');
+  const [valueQuery, setValueQuery] = useState('');
+  /* Appel API */
+  useEffect(() => {
+    axios
+      .get(
+        `https://restcountries.com/v2/name/${valueQuery}
+      `
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        setAllCountries(data);
+      })
+      .catch((err) => console.log(err));
+  }, [valueQuery]);
+
+  return (
+    <div className="cultureContainer">
+      <form className="cultureForm" onSubmit={(e) => e.preventDefault()}>
+        <h2>Chercher un pays :</h2>
+        <input
+          className="cultureSearch"
+          type="text"
+          placeholder="Entrez le nom d'un pays"
+          value={searchCountry}
+          onChange={(e) => setSearchCountry(e.target.value)}
+        />
+        <button type="button" onClick={() => setValueQuery(searchCountry)}>
+          CHERCHER
+        </button>
+      </form>
+      {allCountries
+        .filter((country) => country.name.includes(searchCountry))
+        .map((country) => (
+          <div>
+            <ul className="cultureCard">
+              <h3>Informations :</h3>
+              Pays: {country.name} <br />
+              Capitale: {country.capital} <br />
+              Continent: {country.region} <br />
+              Population: {country.population} <br />
+              <img
+                id="cultureFlags"
+                src={country.flags.svg}
+                alt={country.name}
+              />
+            </ul>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default SearchCulture;
